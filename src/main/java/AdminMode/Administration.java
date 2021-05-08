e29cd2daa7392fb19559fdd65636118c6f970332
lia-ghita <30419671+lia-ghita@users.noreply.github.com>
2021-05-08 22:52:07 +0300

    Admin authentication

--- src/main/java/AdminMode/Administration.java
+++ src/main/java/AdminMode/Administration.java
@@ -15,7 +15,8 @@ public class Administration {
         f.setBounds(CustomSize.width / 3, CustomSize.height / 8, 750, 800);
         f.setTitle("imc Dev Tools");
 
-
+        f.setJMenuBar(new JMenuBar());
+        f.setContentPane(panel);
         //f.pack();
         f.setVisible(true);
 
