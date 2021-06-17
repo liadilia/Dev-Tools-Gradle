1412d167b70188ffb80c62d187cf386c6f573edb
lia-ghita <30419671+lia-ghita@users.noreply.github.com>
2021-06-17 14:51:11 +0300

    cleanup

--- src/main/java/REST/RestEndpoints.java
+++ src/main/java/REST/RestEndpoints.java
@@ -11,18 +11,6 @@ public interface RestEndpoints {
     final String propadminAuth = "LOCALIZATION_AUTH_ENDPOINT";
     final String propadminCreateBundleEndpoint = "LOCALIZATION_BUNDLE_ENDPOINT";
 
-    //  String PROPADMIN_AUTH = "https://propadmin.imc-hosting.com/propadmin/rest/auth";
-    //  String PROPADMIN_CREATE_BUNDLE = "https://propadmin.imc-hosting.com/propadmin/rest/products/ILS/versions/14.6/bundles";
-    //products/ILS/versions/14.6/bundles
-
-    //Jira endpoints
-
-    // /issue supports POST for creating a new issue
-    // String JIRA_ISSUE_ROOT = "https://jira.im-c.de/rest/api/2/issue/";
-    //String JIRA_ISSUE_ROOT = "https://xamplitude.atlassian.net/rest/api/2/issue/";
-    //these endpoints must be appended to the issue root endpoint, after providing the issue ID/Key after the /issue/
-    //https://developer.atlassian.com/server/jira/platform/jira-rest-api-examples/
-
     String JIRA_COMMENT = "/comment/";
 
 
