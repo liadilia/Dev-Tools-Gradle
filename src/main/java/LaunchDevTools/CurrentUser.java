3507621315bcddc997326152db4778533bdabb98
lia-ghita <30419671+lia-ghita@users.noreply.github.com>
2021-07-07 20:28:46 +0300

    add preset values

--- src/main/java/LaunchDevTools/CurrentUser.java
+++ src/main/java/LaunchDevTools/CurrentUser.java
@@ -3,7 +3,7 @@ package LaunchDevTools;
 public class CurrentUser {
     public static String issueKey = "";
     public static String email = "";
-    public static String jiraPassword = "";
+    public static String jiraPassword = "i9At0tY9UOY387P2H7NNDDEE";
     public static String localizationPassword = "";
     public static int role = 0;
 }
